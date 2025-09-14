c87c83bcd15f8e62a73fd6130913e8d65dee088f
MerbahAmine <63507867+MerbahAmine@users.noreply.github.com>
2025-09-14 21:55:05 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,22 @@
+# relationship_app/views.py
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-from Introduction_to_Django.LibraryProject.relationship_app.models import Library
-from . import query_samples
-# Create your views here.
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-def book_list(request):
-      return HttpResponse(query_samples.all_books("A"))
 
-def detail_lebrary(DetailView):
-      model = Library
-      tamplate_name= "relationship_app/library_detail.html"
-      context_object_name="library"
-      
-      def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            context["books"] = self.objct.Book.all()          
-            return context  
+# Class-based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()
+        return context
